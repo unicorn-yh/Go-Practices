9efc17ade379869720b54b85d56c623982905b0d
unicornftk <laiyunghwei@gmail.com>
2022-03-11 07:40:14 +0800

    Create getName.go

---
+++ src/practices/getName.go
@@ -0,0 +1,12 @@
+package main
+import(
+	"fmt"
+	"practices"
+)
+func main(){
+	message := name.Hello("Gladys")
+	fmt.Println(message)
+	//or
+	//var message string
+	//message := name.Hello("Gladys")
+}
